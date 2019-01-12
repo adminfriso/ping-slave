09dc02df55fb4a8983a8ce55407ddc5c1fdd56cd
Friso Modderman <frisomodderman@gmail.com>
2019-01-12 01:36:31 +0100

    fix sending mac address

--- src/index.js
+++ src/index.js
@@ -5,25 +5,25 @@ const socket = io.connect('http://localhost:4000');
 // Receive mac address via execute command
 const {exec} = require('child_process');
 
-const piGetMacAddressPattern = /ether (..:..:..:..:..:..)/gm;
-let piMacAddress = 'error';
+const piGetMacAddressPattern = "/eth0.*\n.*\n.*\n.*ether (..:..:..:..:..:..)/gm";
+const appleGetMacAddressPattern = /ether (..:..:..:..:..:..)/gm;
 exec('ifconfig -a', (err, stdout, stderr) => {
     if (err) {
         // node couldn't execute the command
         return;
     }
-    piMacAddress = new RegExp(piGetMacAddressPattern).exec(stdout)[1];
+    const regResult = new RegExp(appleGetMacAddressPattern).exec(stdout);
+    // send mac address to server
+    socket.emit('macAddress', regResult[1]);
     // the *entire* stdout and stderr (buffered)
     // console.log(`stdout: ${stdout}`);
     // console.log(`stderr: ${stderr}`);
-
 });
-// send mac address to server
-socket.emit('macAddress', piMacAddress);
+
 
 // ----------test and example
 const testDataObjectToServer = {
-    piMacAddress,
+    bernard: 'green',
 };
 // receive data from the server
 socket.on('test', function (data) {
