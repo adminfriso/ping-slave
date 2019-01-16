ce53f1627ff6c69bec91f1a0fb58ad7e6c616b0d
Friso Modderman <frisomodderman@gmail.com>
2019-01-16 23:28:04 +0100

    socket now always sends or receives macAddress
    if server goes down and up
    if node goes down and up

--- src/connections/macAddress.js
+++ src/connections/macAddress.js
@@ -5,17 +5,22 @@ const {exec} = require('child_process');
 module.exports = () => {
     const piGetMacAddressPattern = "/eth0.*\n.*\n.*\n.*ether (..:..:..:..:..:..)/gm";
     const appleGetMacAddressPattern = /ether (..:..:..:..:..:..)/gm;
-    // Receive mac address via execute command and return it on the socket
-    exec('ifconfig -a', (err, stdout, stderr) => {
-        if (err) {
-            // node couldn't execute the command
-            return;
-        }
-        const regResult = new RegExp(appleGetMacAddressPattern).exec(stdout);
-        // send mac address to server
-        socket.emit('mac-address', regResult[1]);
-        // the *entire* stdout and stderr (buffered)
-        // console.log(`stdout: ${stdout}`);
-        // console.log(`stderr: ${stderr}`);
-    })
+    
+    socket.on('mac-address', function () {
+        // Receive mac address via execute command and return it on the socket
+        exec('ifconfig -a', (err, stdout, stderr) => {
+            if (err) {
+                // node couldn't execute the command
+                return;
+            }
+
+            const regResult = new RegExp(appleGetMacAddressPattern).exec(stdout);
+            // send mac address to server
+            socket.emit('mac-address', regResult[1]);
+            // the *entire* stdout and stderr (buffered)
+            // console.log(`stdout: ${stdout}`);
+            // console.log(`stderr: ${stderr}`);
+        });
+    });
+
 };
