12da2b1a65a18a93b810aa6382597b43fc2fa099
Friso Modderman <frisomodderman@gmail.com>
2019-01-19 21:38:28 +0100

    build failsafe in input
    we never want the application to fail if something happens and the application crashes

--- src/connections/executeCommand.js
+++ src/connections/executeCommand.js
@@ -4,15 +4,20 @@ const {exec} = require('child_process');
 
 module.exports = () => {
 // receive data from the server
-    socket.on('execute-command', function (data) {
+    socket.on('execute-command', (data) => {
+        if (data == null){
+            socket.emit('execute-command', null);
+            return;
+        }
         exec(data.command, (err, stdout, stderr) => {
             if (err) {
                 // node couldn't execute the command
                 return;
             }
-            let response = {};
-            response.stdout = stdout;
-            response.stderr = stderr;
+            let response = {
+                stdout,
+                stderr,
+            };
             // send data to the server
             socket.emit('execute-command', response);
         });
