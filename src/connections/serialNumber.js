a205895a53c05f84a6b110cf93c1598359ada1a2
Friso Modderman <frisomodderman@gmail.com>
2019-03-12 19:45:48 +0100

    updated env configuration

--- src/connections/serialNumber.js
+++ src/connections/serialNumber.js
@@ -1,31 +1,37 @@
 'use strict';
 
 module.exports = () => {
-    // const piPattern = /(\w*)/gm;
-    const piCommand = 'cat /proc/cpuinfo | grep ^Serial | cut -d":" -f2';
-    const applePattern = /"(\w{12})"/gm;
-    const appleCommand = 'ioreg -l | grep IOPlatformSerialNumber';
+  const piPattern = /"(\S*)"/gm;
+  const piCommand = 'cat /proc/cpuinfo | grep ^Serial | cut -d":" -f2';
+  const applePattern = /"(\w{12})"/gm;
+  const appleCommand = 'ioreg -l | grep IOPlatformSerialNumber';
 
-    const pattern = applePattern;
-    const command = appleCommand;
+  let pattern = piPattern;
+  let command = piCommand;
 
-    services.socket.on('serial-number', function () {
-        // Receive mac address via execute command and return it on the socket
-        exec(command, (err, stdout, stderr) => {
-            if (err) {
-                // node couldn't execute the command
-                return;
-            }
-            const regResult = new RegExp(pattern).exec(stdout);
-            // send serial number to server
-            let response = {
-              serialNumber: regResult[1],
-            };
-            services.socket.emit('serial-number', response);
-            // the *entire* stdout and stderr (buffered)
-            // console.log(`stdout: ${stdout}`);
-            // console.log(`stderr: ${stderr}`);
-        });
+  if (process.env.DEVICE === 'apple') {
+    pattern = applePattern;
+    command = appleCommand;
+  }
+
+
+  services.socket.on('serial-number', function () {
+    // Receive mac address via execute command and return it on the socket
+    exec(command, (err, stdout, stderr) => {
+      if (err) {
+        // node couldn't execute the command
+        return;
+      }
+      const regResult = new RegExp(pattern).exec(stdout);
+      // send serial number to server
+      let response = {
+        serialNumber: regResult[1],
+      };
+      services.socket.emit('serial-number', response);
+      // the *entire* stdout and stderr (buffered)
+      // console.log(`stdout: ${stdout}`);
+      // console.log(`stderr: ${stderr}`);
     });
+  });
 
 };
