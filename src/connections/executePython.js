662fc434ff54a61421b59ae96942081f908a66f1
Friso Modderman <frisomodderman@gmail.com>
2019-05-06 16:44:09 +0200

    Update executePython.js

--- src/connections/executePython.js
+++ src/connections/executePython.js
@@ -6,7 +6,7 @@ module.exports = () => {
   console.log('starting python');
   let pyShell = new PythonShell('./python/PiMaster3_1.py', {
     mode: 'text',
-    pythonPath: '/usr/bin/python'
+    pythonPath: '/usr/bin/python2.7'
   });
   console.log('started python');
 
@@ -31,16 +31,17 @@ module.exports = () => {
 
     // console.log(pyShell.stdout);
     let responseMessage = '';
-    console.log('setting timer reply with timeout of 50ms');
-    let responseTimer = setTimeout(reply, 50);
+    // console.log('setting timer reply with timeout of 50ms');
+    // let responseTimer = setTimeout(reply, 50);
     pyShell.on('message', function (message) {
       // handle message (a line of text from stdout)
       console.log('response from python: ' + message);
       responseMessage = responseMessage + message;
-      console.log('clearing timer');
-      clearTimeout(responseTimer);
-      console.log('setting timer reply with timeout of 50ms');
-      responseTimer = setTimeout(reply, 50);
+      reply();
+      // console.log('clearing timer');
+      // clearTimeout(responseTimer);
+      // console.log('setting timer reply with timeout of 50ms');
+      // responseTimer = setTimeout(reply, 50);
     });
 
     function reply() {
