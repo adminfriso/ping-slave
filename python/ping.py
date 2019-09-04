04f97def42d69ebfb0ea2f4de3c63588550b6bf3
GvBon <gijs@gijsvanbon.nl>
2019-09-04 15:30:02 +0200

    Update ping.py

--- python/ping.py
+++ python/ping.py
@@ -28,7 +28,7 @@ try:
 except:
     from pygame import mixer
 mixer.init()
-mixer.set_num_channels(16)#default is 8
+mixer.set_num_channels(30)#default is 8
 ## import PIL
 from PIL import Image
 from PIL import ImageChops
