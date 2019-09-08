40bb8d2e080201fa5c9ec76fa0e146d0e30cb80e
GvBon <gijs@gijsvanbon.nl>
2019-09-08 21:20:29 +0200

    Update ping.py

--- python/ping.py
+++ python/ping.py
@@ -156,8 +156,8 @@ def showLeds (im,frame):
         strip.setPixelColor(4, led1)
         strip.setPixelColor(5, led2)
         strip.setPixelColor(13, led2)
-    strip.setPixelColor(14, led1)
-    strip.setPixelColor(15, led0)
+        strip.setPixelColor(14, led1)
+        strip.setPixelColor(15, led0)
     strip.show()
 
 class LightSlave(threading.Thread):
