19e2c9292c535359515bb0779c710adfb5f78ff0
Friso Modderman <frisomodderman@gmail.com>
2019-10-15 19:05:44 +0200

    Update updateNtp.sh

--- updateNtp.sh
+++ updateNtp.sh
@@ -4,15 +4,18 @@ exec sudo -u root /bin/sh - << eof
     #code if found
     if grep -Fxq "server 192.168.8.50" /etc/ntp.conf
     then
+      echo "configration is done, shutting down"
       # code if found
       shutdown
     else
       #code if found
+      echo "configuring ntp"
       echo "server 192.168.8.50" >> /etc/ntp.conf
       service ntp reload
     fi
   else
     #code if not found
+    echo "installing ntp"
     apt install ntp -y
     ./updateNtp.sh
   fi
