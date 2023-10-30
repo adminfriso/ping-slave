50612570d1990a2ee3d81224e87fb31a47c958ed
Friso Modderman <frisomodderman@gmail.com>
2023-10-30 17:40:46 +0100

    Update pingButton.py

--- python/pingButton.py
+++ python/pingButton.py
@@ -375,7 +375,7 @@ if __name__ == '__main__':
             led.blink(0, 0, 0.1, 0.3, 1, True)  # ontime, offtime, fadeintime, fade out time, n-times, in background
             sock = socket.socket(socket.AF_INET,socket.SOCK_DGRAM)
             tempString=subprocess.call("cat /proc/cpuinfo | grep ^Serial | cut -d':' -f2", shell=True)
-            sock.sendto(tempString.encode(), ("192.168.0.255",7149))
+            sock.sendto(tempString.encode(), ("192.168.11.255",7149))
 
         try:
             com = raw_input("s/i/h/w/e/c/p,file,volume(,time)>")
